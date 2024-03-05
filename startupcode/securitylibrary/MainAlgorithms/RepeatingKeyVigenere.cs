0229b135f70ca921587e999eaa24ed218999120c
engy medhat <mengy289@gmail.com>
2024-03-05 23:39:06 +0200

    repeatingkey

--- startupcode/securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ startupcode/securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -10,7 +10,7 @@ namespace SecurityLibrary
     {
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public string Decrypt(string cipherText, string key)
