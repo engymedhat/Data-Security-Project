4d4257f309b287c13b8ddf26f4d39de6d7d27aa2
engy medhat <mengy289@gmail.com>
2024-03-05 23:38:14 +0200

    repeatingkey

--- startupcode/securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ startupcode/securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -15,12 +15,75 @@ namespace SecurityLibrary
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            char[] keyStream = new char[cipherText.Length];
+            char[] decrypted = new char[cipherText.Length];
+            bool iscompleted = false;
+            int index = 0;
+            int keyIndex, cipherIndex;
+
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                if (i == key.Length)
+                {
+                    index = 0;
+                    iscompleted = true;
+                }
+                if (!iscompleted)
+                {
+                    keyStream[i] = key[i];
+                }
+                else
+                {
+                    keyStream[i] = key[index];
+                }
+                index++;
+                index %= decrypted.Length;
+                cipherIndex = cipherText[i] - 'A';
+                keyIndex = keyStream[i] - 'a';
+                decrypted[i] = (char)(((cipherIndex - keyIndex+26)%26;
+            }
+            string result = new string(decrypted);
+            return result;
+        }
+    }
+}
         }
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            char[] keyStream = new char[plainText.Length];
+            char[] encrypted = new char[plainText.Length];
+            bool iscompleted = false;
+            int index = 0;
+            int keyIndex, plainIndex;
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                if (i == key.Length)
+                {
+                    index = 0;
+                    iscompleted = true;
+                }
+                if (!iscompleted)
+                {
+                    keyStream[i] = key[i];
+                }
+                else
+                {
+                    keyStream[i] = key[index];
+                }
+                index++;
+                index %= plainText.Length;
+                plainIndex = plainText[i] - 'a';
+                keyIndex = keyStream[i] - 'a';
+                encrypted[i] = (char)(((plainIndex + keyIndex) % 26) + 'A');
+            }
+            string result = new string(encrypted);
+            return result;
         }
+    }
+}
     }
 }
